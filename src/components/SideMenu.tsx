5be3439ad7afbe8e6a3544e762be369f89c3ab17
David Karrick <the.white.wolf.is.1337@gmail.com>
2023-05-27 02:38:22 -0600

    Refactor: Small tweaks to side menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,16 +1,4 @@
-import { JSXElement, For, createSignal } from "solid-js";
-import { Collapsible } from "@kobalte/core";
-import ChevronDownIcon from "~icons/carbon/chevron-down";
-import GamingIcon from "~icons/ph/game-controller";
-import SportsIcon from "~icons/ph/baseball-light";
-import BusinessIcon from "~icons/ph/chart-line-up-light";
-import CryptoIcon from "~icons/icon-park-outline/blockchain";
-import TelevisionIcon from "~icons/ph/television";
-import CelebrityIcon from "~icons/ph/star-light";
-import MoreTopicsIcon from "~icons/mdi/dots-horizontal-circle-outline";
-
-import HomeIcon from "~icons/ph/house";
-import PopularIcon from "~icons/ph/arrow-circle-up-right";
+import { For, JSXElement, createEffect, createSignal, onCleanup } from "solid-js";
 import {
 	BusinessTopicsItem as BusinessItem,
 	CelebrityItems,
@@ -48,12 +36,12 @@ const feedsSection = {
 	items: [
 		{
 			href: "/?feed=home",
-			icon: <HomeIcon class="font-normal text-xl/5" />,
+			icon: <div class="home font-normal text-xl/5" />,
 			title: "Home"
 		},
 		{
 			href: "/r/popular",
-			icon: <PopularIcon class="font-normal text-xl/5" />,
+			icon: <div class="Popular font-normal text-xl/5" />,
 			title: "Popular"
 		}
 	]
@@ -107,37 +95,37 @@ const recentSection = {
 const TopicsMenuSections = [
 	{
 		name: "Gaming",
-		icon: <GamingIcon class="font-normal text-xl/5" />,
+		icon: <div class="gaming font-normal text-xl/5 h-5 w-5" />,
 		items: GamingItems
 	},
 	{
 		name: "Sports",
-		icon: <SportsIcon class="font-normal text-xl/5" />,
+		icon: <div class="sports font-normal text-xl/5 h-5 w-5" />,
 		items: SportsItem
 	},
 	{
 		name: "Business, Economics, and Finance",
-		icon: <BusinessIcon class="font-normal text-xl/5" />,
+		icon: <div class="business font-normal text-xl/5 h-5 w-5" />,
 		items: BusinessItem
 	},
 	{
 		name: "Crypto",
-		icon: <CryptoIcon class="font-normal text-xl/5" />,
+		icon: <div class="crypto font-normal text-xl/5 h-5 w-5" />,
 		items: CryptoItems
 	},
 	{
 		name: "Television",
-		icon: <TelevisionIcon class="font-normal text-xl/5" />,
+		icon: <div class="tv font-normal text-xl/5 h-5 w-5" />,
 		items: TelevisionItems
 	},
 	{
 		name: "Celebrity",
-		icon: <CelebrityIcon class="font-normal text-xl/5" />,
+		icon: <div class="star font-normal text-xl/5 h-5 w-5" />,
 		items: CelebrityItems
 	},
 	{
 		name: "More Topics",
-		icon: <MoreTopicsIcon class="font-normal text-xl/5" />,
+		icon: <div class="dots font-normal text-xl/5 h-5 w-5" />,
 		items: MoreItems
 	}
 ];
@@ -146,7 +134,7 @@ const SideMenuItem = (props: SideMenuItemProps) => {
 	return (
 		<a
 			href={props.href}
-			class="px-6 py-2 flex flex-row align-center text-sm leading-[1.125rem] font-medium relative hover:bg-gray-100 text-[#1C1C1C]"
+			class="pl-6 py-2 flex flex-row align-center text-sm leading-[1.125rem] font-medium relative hover:bg-gray-100 text-[#1C1C1C]"
 		>
 			{props.icon}
 			<span class="ml-2 text-sm leading-[1.125rem] overflow-hidden grow text-left text-ellipsis whitespace-nowrap max-width-[13rem]">
@@ -159,7 +147,7 @@ const SideMenuItem = (props: SideMenuItemProps) => {
 const SideMenuSection = (props: SideMenuSectionProps) => {
 	return (
 		<>
-			<h3 class="px-6 pt-4 pb-2 font-medium text-[10px]/6 text-[#878A8C]">{props.title}</h3>
+			<h3 class="px-6 pt-2 pb-2 font-medium text-[10px]/6 text-[#878A8C]">{props.title}</h3>
 			<ul>
 				<For each={props.items}>
 					{(item) => (
@@ -173,20 +161,45 @@ const SideMenuSection = (props: SideMenuSectionProps) => {
 	);
 };
 
-const DropDownMenu = (props: DropDownMenuProps) => {
+const Collapsible = (props: DropDownMenuProps) => {
+	const [isOpen, setIsOpen] = createSignal(false);
+	let contentRef: HTMLDivElement | undefined;
+
+	function toggleCollapsible() {
+		setIsOpen(!isOpen());
+	}
+
+	onCleanup(() => {
+		// Reset height to auto on component cleanup
+		if (contentRef) {
+			contentRef.style.height = "auto";
+		}
+	});
+
+	createEffect(() => {
+		// Update height on isOpen change
+		if (contentRef) {
+			contentRef.style.height = isOpen() ? `${contentRef.scrollHeight}px` : "0";
+		}
+	});
+
 	return (
-		<Collapsible.Root class="collapsible">
-			<Collapsible.Trigger class="w-full collapsible__trigger ">
-				<div class="px-6 py-2 flex flex-row align-center text-sm leading-[1.125rem] font-medium relative hover:bg-gray-100 text-[#1C1C1C]">
-					<div class="h-5 w-5">{props.icon}</div>
-					<span class="ml-2 my-auto text-sm leading-[1.125rem] overflow-hidden grow align-middle text-left text-ellipsis whitespace-nowrap w-[10rem] ">
+		<div class="w-[16.875rem]">
+			<button
+				type="button"
+				onClick={toggleCollapsible}
+				class="flex items-center justify-between w-[16.875rem] text-sm font-medium text-gray-700 hover:bg-gray-100 bg-white focus:outline-none transition-all duration-300"
+			>
+				<div class="px-4 py-2 flex flex-row align-center text-sm leading-[1.125rem] font-medium relative  text-[#1C1C1C]">
+					<div class="ml-2 h-5 w-5">{props.icon}</div>
+					<span class="ml-2 px-1 my-auto text-sm leading-[1.125rem] overflow-hidden grow align-middle text-left text-ellipsis whitespace-nowrap w-[11.15rem] ">
 						{props.name}
 					</span>
 
-					<ChevronDownIcon class="flex justify-end h-5 w-5 collapsible__trigger-icon" />
+					<div class="downChevron flex justify-end h-6 w-6 mt-1" />
 				</div>
-			</Collapsible.Trigger>
-			<Collapsible.Content class="block collapsible__content ml-13 mr-5 mt-0 mb-2">
+			</button>
+			<div ref={contentRef} class="overflow-hidden transition-all duration-300">
 				<For each={props.items}>
 					{(item) => (
 						<a
@@ -199,17 +212,17 @@ const DropDownMenu = (props: DropDownMenuProps) => {
 						</a>
 					)}
 				</For>
-			</Collapsible.Content>
-		</Collapsible.Root>
+			</div>
+		</div>
 	);
 };
 
 const TopicsMenuSection = () => {
 	return (
 		<>
-			<h3 class="px-6 pt-4 pb-2 font-medium text-[10px]/6 text-[#878A8C]">TOPICS</h3>
+			<h3 class="px-6 pt-2 pb-2 font-medium text-[10px]/6 text-[#878A8C]">TOPICS</h3>
 			<ul>
-				<For each={TopicsMenuSections}>{(menu) => <DropDownMenu {...menu} />}</For>
+				<For each={TopicsMenuSections}>{(menu) => <Collapsible {...menu} />}</For>
 			</ul>
 		</>
 	);
@@ -217,7 +230,7 @@ const TopicsMenuSection = () => {
 
 export default function SideMenu() {
 	return (
-		<section class="w-60 flex flex-col bg-white">
+		<section class="w-[16.875rem] flex flex-col bg-white">
 			<SideMenuSection {...feedsSection} />
 			<SideMenuSection {...recentSection} />
 			<TopicsMenuSection />
